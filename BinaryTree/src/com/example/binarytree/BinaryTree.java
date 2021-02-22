8ba8003c9a881eb7cee690592c35c95df2c2da0e
军杰 <weijunjievip@yeah.net>
2021-02-22 10:44:42 +0800

    添加使用栈结构遍历二叉树的方法，以及广度优先遍历二叉树的方法

--- BinaryTree/src/com/example/binarytree/BinaryTree.java
+++ BinaryTree/src/com/example/binarytree/BinaryTree.java
@@ -1,6 +1,8 @@
 package com.example.binarytree;
 
 import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
 
 public class BinaryTree<T> {
 
@@ -27,7 +29,7 @@ public class BinaryTree<T> {
     /**
      * 使用递归的方式前序遍历二叉树
      *
-     * @param treeNode 要被遍历的二叉树
+     * @param treeNode 要被遍历的二叉树的根节点
      */
     public void preOrderTraversal(TreeNode<T> treeNode) {
         if (treeNode == null) {
@@ -41,7 +43,7 @@ public class BinaryTree<T> {
     /**
      * 中序遍历二叉树
      *
-     * @param treeNode 要被遍历的二叉树
+     * @param treeNode 要被遍历的二叉树的根节点
      */
     public void inOrderTraversal(TreeNode<T> treeNode) {
         if (treeNode == null) {
@@ -55,7 +57,7 @@ public class BinaryTree<T> {
     /**
      * 后序遍历二叉树
      *
-     * @param treeNode 要被遍历的二叉树
+     * @param treeNode 要被遍历的二叉树的根节点
      */
     public void postOrderTraversal(TreeNode<T> treeNode) {
         if (treeNode == null) {
@@ -64,6 +66,94 @@ public class BinaryTree<T> {
         postOrderTraversal(treeNode.leftChild);
         postOrderTraversal(treeNode.rightChild);
         System.out.print(treeNode.data + " ");
+    }
+
+    /**
+     * 使用栈结构前序遍历二叉树
+     *
+     * @param treeNode 要被遍历的二叉树的根节点
+     */
+    public void preOrderTraversalByStack(TreeNode<T> treeNode) {
+        Stack<TreeNode<T>> stack = new Stack<>();
+        TreeNode<T> root = treeNode;
+        while (root != null || !stack.isEmpty()) {
+            while (root != null) {
+                stack.push(root);
+                System.out.print(root.data + " ");
+                root = root.leftChild;
+            }
+            if (!stack.isEmpty()) {
+                root = stack.pop();
+                root = root.rightChild;
+            }
+        }
+
+    }
+
+    /**
+     * 使用栈结构中序遍历二叉树
+     *
+     * @param treeNode 要被遍历的二叉树的根节点
+     */
+    public void inOrderTraversalByStack(TreeNode<T> treeNode) {
+        Stack<TreeNode<T>> stack = new Stack<>();
+        TreeNode<T> root = treeNode;
+        while (root != null || !stack.isEmpty()) {
+            while (root != null) {
+                stack.push(root);
+                root = root.leftChild;
+            }
+            if (!stack.isEmpty()) {
+                root = stack.pop();
+                System.out.print(root.data + " ");
+                root = root.rightChild;
+            }
+        }
+    }
+
+    /**
+     * 使用栈结构后序遍历二叉树
+     *
+     * @param treeNode 要被遍历的二叉树的根节点
+     */
+    public void postOrderTraversalByStack(TreeNode<T> treeNode) {
+        Stack<TreeNode<T>> stack = new Stack<>();
+        Stack<TreeNode<T>> printStack = new Stack<>(); //存放打印结果的栈
+        TreeNode<T> root = treeNode;
+        while (root != null || !stack.isEmpty()) {
+            while (root != null) {
+                stack.push(root);
+                printStack.push(root);
+                root = root.rightChild;
+            }
+            if (!stack.isEmpty()) {
+                root = stack.pop();
+                root = root.leftChild;
+            }
+        }
+        while (!printStack.isEmpty()) {
+            System.out.print(printStack.pop().data + " ");
+        }
+    }
+
+    /**
+     * 利用队列层序遍历二叉树
+     *
+     * @param treeNode 要被遍历的二叉树的根节点
+     */
+    public void levelOrderTraversal(TreeNode<T> treeNode) {
+        Queue<TreeNode<T>> queue = new LinkedList<>();
+        queue.offer(treeNode);
+        while (!queue.isEmpty()) {
+            TreeNode<T> root = queue.poll();
+            System.out.print(root.data + " ");
+            if (root.leftChild != null) {
+                queue.offer(root.leftChild);
+            }
+            if (root.rightChild != null) {
+                queue.offer(root.rightChild);
+            }
+        }
     }
 
 }
--- BinaryTree/src/com/example/binarytree/BinaryTreeTest.java
+++ BinaryTree/src/com/example/binarytree/BinaryTreeTest.java
@@ -8,6 +8,8 @@ public class BinaryTreeTest {
     public static void main(String[] args) {
         LinkedList<Integer> linkedList = new LinkedList<>(Arrays.asList(3, 2, 9, null, null, 10, null, null, 8, null, 4));
 
+//        LinkedList<String> linkedList = new LinkedList<>(Arrays.asList("傲慢与偏见", "茶花女", "局外人", null, null, "飘", null, null, "少年维特的烦恼", null, "百年孤独"));
+
         BinaryTree<Integer> binaryTree = new BinaryTree<>();
         TreeNode<Integer> treeNode = binaryTree.createTree(linkedList);
 
@@ -19,6 +21,22 @@ public class BinaryTreeTest {
         System.out.println();
         System.out.println("后序遍历：");
         binaryTree.postOrderTraversal(treeNode);
+
+        System.out.println();
+        System.out.println();
+        System.out.println("利用栈结构前序遍历二叉树：");
+        binaryTree.preOrderTraversalByStack(treeNode);
+        System.out.println();
+        System.out.println("利用栈结构中序遍历二叉树：");
+        binaryTree.inOrderTraversalByStack(treeNode);
+        System.out.println();
+        System.out.println("利用栈结构后序遍历二叉树：");
+        binaryTree.postOrderTraversalByStack(treeNode);
+
+        System.out.println();
+        System.out.println();
+        System.out.println("层序遍历二叉树：");
+        binaryTree.levelOrderTraversal(treeNode);
     }
 
 }
