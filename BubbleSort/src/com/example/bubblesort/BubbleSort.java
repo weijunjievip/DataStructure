a3e9681110e6d75ee1e748c8dee7dad91c74b933
军杰 <weijunjievip@yeah.net>
2021-02-26 10:24:28 +0800

    定义有序边界，提示算法的排序性能

--- BubbleSort/src/com/example/bubblesort/BubbleSort.java
+++ BubbleSort/src/com/example/bubblesort/BubbleSort.java
@@ -22,10 +22,11 @@ public class BubbleSort {
      * @param array 要排序的数组
      */
     public static void ascSort(int[] array) {
-        for (int i = 0; i < array.length - 1; i++) {
+        int len = array.length - 1;
+        for (int i = 0; i < len; i++) {
             //声明一个变量，用来判断当前数组中的元素是否已经是有序的了，默认为true，表示是有序的。
             boolean isSorted = true;
-            for (int j = 0; j < array.length - 1 - i; j++) {
+            for (int j = 0; j < len - i; j++) {
                 if (array[j] > array[j + 1]) {
                     swap(array, j, j + 1);
                     //如果在比较的过程中数组中的元素发生了交换操作，则说明数组仍旧是无序的，那就继续循环比较
@@ -45,14 +46,68 @@ public class BubbleSort {
      * @param array 要排序的数组
      */
     public static void descSort(int[] array) {
-        for (int i = 0; i < array.length - 1; i++) {
+        int len = array.length - 1;
+        for (int i = 0; i < len; i++) {
             boolean isSorted = true;
-            for (int j = 0; j < array.length - 1 - i; j++) {
+            for (int j = 0; j < len - i; j++) {
                 if (array[j] < array[j + 1]) {
                     swap(array, j, j + 1);
                     isSorted = false;
                 }
             }
+            if (isSorted) {
+                break;
+            }
+        }
+    }
+
+    /**
+     * 优化后的升序排序
+     * 在排序的过程中有可能经过几轮的比较之后，数组已经是有序的了，或者数组后半部分的元素已经是有序的了，如果不进行优化，按照原先的循环条件按个比较的话。
+     * 那么有序的部分其实在白白浪费时间。因此我们在这里定义了一个有序边界，数组中有序边界之后的部分已经是有序的了，因此就没有在参与循环比较的必要了，
+     * 只有有序边界之前的部分才继续参与循环比较。这样可以节省排序时间，提示算法性能。
+     *
+     * @param array 待排序的数组
+     */
+    public static void ascSortOptimize(int[] array) {
+        int len = array.length - 1;
+        int lastExchangeIndex = 0;//记录数组中最后一次元素交换的位置
+        int sortBorder = array.length - 1;//数组中元素的有序边界
+        for (int i = 0; i < len; i++) {
+            boolean isSorted = true;
+            for (int j = 0; j < sortBorder; j++) {//数组中有序边界之后的元素都是有序的，因此就没有再参与比较的必要了。
+                if (array[j] > array[j + 1]) {
+                    swap(array, j, j + 1);
+                    isSorted = false;
+                    lastExchangeIndex = j;//将发生了元素交换的角标位赋值给lastExchangeIndex。
+                }
+            }
+            sortBorder = lastExchangeIndex;//数组中最后一次进行元素交换的位置就是数组中元素的有序边界
+            if (isSorted) {
+                break;
+            }
+        }
+    }
+
+    /**
+     * 优化后的降序排序
+     *
+     * @param array 待优化的数组
+     */
+    public static void descSortOptimize(int[] array) {
+        int len = array.length - 1;
+        int lastExchangeIndex = 0;
+        int sortBorder = array.length - 1;
+        for (int i = 0; i < len; i++) {
+            boolean isSorted = true;
+            for (int j = 0; j < sortBorder; j++) {
+                if (array[j] < array[j + 1]) {
+                    swap(array, j, j + 1);
+                    isSorted = false;
+                    lastExchangeIndex = j;
+                }
+            }
+            sortBorder = lastExchangeIndex;
             if (isSorted) {
                 break;
             }
--- BubbleSort/src/com/example/bubblesort/BubbleSortTest.java
+++ BubbleSort/src/com/example/bubblesort/BubbleSortTest.java
@@ -5,13 +5,23 @@ import java.util.Arrays;
 public class BubbleSortTest {
 
     public static void main(String[] args) {
-        final int[] array = {3, 2, 9, 1, 0, 5, 4, 6, 7, 8, 3};
+        int[] array = {3, 2, 9, 1, 0, 5, 4, 6, 7, 8, 3};
         System.out.println("排序前：");
         System.out.println(Arrays.toString(array));
         BubbleSort.ascSort(array);
         System.out.println("升序排序后：");
         System.out.println(Arrays.toString(array));
         BubbleSort.descSort(array);
+        System.out.println("降序排序后：");
+        System.out.println(Arrays.toString(array));
+
+        array = new int[]{3, 4, 2, 1, 5, 6, 7, 8};
+        System.out.println("排序前：");
+        System.out.println(Arrays.toString(array));
+        BubbleSort.ascSortOptimize(array);
+        System.out.println("升序排序后：");
+        System.out.println(Arrays.toString(array));
+        BubbleSort.descSortOptimize(array);
         System.out.println("降序排序后：");
         System.out.println(Arrays.toString(array));
     }
